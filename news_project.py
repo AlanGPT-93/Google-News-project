2fe25e994d904c64eaa87117a532d028f1877d65
Alan García <alan@test.com>
2021-02-15 20:14:17 -0600

    Json files deleted

--- news_project.py
+++ news_project.py
@@ -30,6 +30,14 @@ subprocess.run(['sh','news_extractor.sh', search_topic,execution_year, start_exe
 
 
 ## Import Stop Words
-#from nltk.corpus import stopwords
-#sw_eng = stopwords.words('english') ## English Stop Words
-#sw_esp = stopwords.words('spanish') ## Spanish Stop Words
+from nltk.corpus import stopwords
+sw_eng = stopwords.words('english') ## English Stop Words
+sw_esp = stopwords.words('spanish') ## Spanish Stop Words
+
+
+def remove_punctuation(text):
+    import re
+    text = text.lower()
+    text = re.sub(r'https\S+|@\S+|[^\w\s]|_',' ',text)
+    text = re.sub(r'\s+'," ",text).strip() 
+    return text
